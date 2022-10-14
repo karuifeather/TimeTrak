cd008eeb948552ae9dc96d918132885cae8b9dcd
Kevin Navarrete <knavarreteres@stu.jsu.edu>
2022-10-14 12:03:50 -0500

    Added Kevin Navarrete name

---
+++ source/cs310-teamproject-fa22/src/edu/jsu/mcis/cs310/tas_fa22/Punch.java
@@ -0,0 +1,19 @@
+package edu.jsu.mcis.cs310.tas_fa22;
+
+import java.time.LocalDateTime;
+
+public class Punch {
+
+    public void Punch(int terminalid, Badge badge, EventType punchtype) {
+
+    }
+
+    public void Punch(int id, 
+            int terminalid, 
+            Badge badge, 
+            LocalDateTime originaltimestamp, 
+            EventType punchtype) {
+
+    }
+
+}
