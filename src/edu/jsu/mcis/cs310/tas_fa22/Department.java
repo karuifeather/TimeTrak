d596669bf35f7db36d63c1f5b51b5b4b9a82cc08
wahickssware84 <wahickssware84@gmail.com>
2022-10-26 12:06:31 -0500

    Added method toString() to Department.java.

--- src/edu/jsu/mcis/cs310/tas_fa22/Department.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/Department.java
@@ -1,21 +1,36 @@
 package edu.jsu.mcis.cs310.tas_fa22;
 
 public class Department {
+
     private final String id, description, terminalid;
-    
+
     public Department(String id, String description, String terminalid) {
         this.id = id;
         this.description = description;
         this.terminalid = terminalid;
     }
-    
+
     public String getId() {
         return id;
     }
+
     public String getDescription() {
         return description;
     }
+
     public String getTerminalId() {
         return terminalid;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+
+        s.append('#').append(id).append(' ');
+        s.append('(').append(description).append(')');
+        s.append("Terminal ID: ").append(terminalid).append(' ');
+
+        return s.toString();
     }
 }
