10d0b1b73923924b2bf5dd01f68c5cd86552762e
wahickssware84 <wahickssware84@gmail.com>
2022-10-31 19:54:12 -0500

    Partial implementation of DepartmentDAO.java.

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/DepartmentDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/DepartmentDAO.java
@@ -1,7 +1,37 @@
 package edu.jsu.mcis.cs310.tas_fa22.dao;
 import edu.jsu.mcis.cs310.tas_fa22.*;
 import java.sql.*;
+import java.util.*;
 
 public class DepartmentDAO {
-    // CODE WILL BE INSERTED HERE
+    private static final String QUERY_FIND_ID = "SELECT * FROM employee WHERE id = ?";
+    private final DAOFactory daoFactory;
+    
+    // Constructor
+    DepartmentDAO (DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+    
+    public Department find(int id) {
+        Department department = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        try {
+            Connection conn = daoFactory.getConnection();
+            if (conn.isValid(0)) {
+                ps = conn.ps(QUERY_FIND_ID);
+                ps.setInt(1, id);
+                boolean hasResults = ps.execute();
+                
+                if (hasResults) {
+                    rs = ps.getResultSet();
+                }
+                while (rs.next()) {
+                    // --- UNFINISHED ---
+                    department = new Department(id, description, terminalid);
+                }
+            }
+        }
+    }
 }
