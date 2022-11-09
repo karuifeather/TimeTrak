0c6b1e890871374176ed19189e884f15084cd2bd
Kevin Navarrete <knavarreteres@stu.jsu.edu>
2022-11-09 12:07:14 -0600

    getPunchListAsJSON method complete and getAbsenteeismDAO() method added in DAOFactory

---
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/AbsenteeismDAO.java
@@ -0,0 +1,12 @@
+package edu.jsu.mcis.cs310.tas_fa22.dao;
+
+import edu.jsu.mcis.cs310.tas_fa22.*;
+import java.sql.*;
+
+public class AbsenteeismDAO {
+    private final DAOFactory daoFactory;
+    
+    AbsenteeismDAO(DAOFactory daofactory){
+        this.daoFactory =
+    }
+}
--- src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOUtility.java
@@ -2,7 +2,6 @@ package edu.jsu.mcis.cs310.tas_fa22.dao;
 
 import edu.jsu.mcis.cs310.tas_fa22.*;
 import java.time.*;
-import java.util.*;
 import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 import org.json.simple.*;
@@ -20,21 +19,23 @@ public final class DAOUtility {
 
     public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist) {
         ArrayList<HashMap<String, String>> jsonData = new ArrayList<>();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("EEE MM/dd/yyyy HH:mm:ss");
 
         for (Punch punch : dailypunchlist) {
             HashMap<String, String> punchData = new HashMap<>();
 
             punchData.put("id", String.valueOf(punch.getId()));
-            punchData.put("badgeid", String.valueOf(punch.getBadge()));
+            punchData.put("badgeid", String.valueOf(punch.getBadge().getId()));
             punchData.put("terminalid", String.valueOf(punch.getTerminalid()));
             punchData.put("punchtype", String.valueOf(punch.getPunchtype()));
             punchData.put("adjustmenttype", String.valueOf(punch.getAdjustmenttype()));
-            punchData.put("originaltimestamp", String.valueOf(punch.getOriginaltimestamp()));
-            punchData.put("adjustedtimestamp", String.valueOf(punch.getAdjustedtimestamp()));
+            punchData.put("originaltimestamp", punch.getOriginaltimestamp().format(format).toUpperCase());
+            punchData.put("adjustedtimestamp", punch.getAdjustedtimestamp().format(format).toUpperCase());
 
             jsonData.add(punchData);
 
         }
+
         String json = JSONValue.toJSONString(jsonData);
         return json;
 
