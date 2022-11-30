d4670f2d596a44e5274b0cb3dc30feb0d32591c2
wahickssware84 <wahickssware84@gmail.com>
2022-11-30 17:37:38 -0600

    Reformatted main class

--- src/edu/jsu/mcis/cs310/tas_fa22/Main.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/Main.java
@@ -1,8 +1,7 @@
 /*
     Authors: Aashaya Aryal, Jakob Skipper, William Hicks, Caleb Watts, James Highfield, Kevin Navarrete
     
-*/
-
+ */
 package edu.jsu.mcis.cs310.tas_fa22;
 
 import edu.jsu.mcis.cs310.tas_fa22.dao.*;
@@ -10,20 +9,17 @@ import edu.jsu.mcis.cs310.tas_fa22.dao.*;
 public class Main {
 
     public static void main(String[] args) {
-        
-        // test database connectivity; get DAOs
 
+        // test database connectivity; get DAOs
         DAOFactory daoFactory = new DAOFactory("tas.jdbc");
         BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
-        
-        // find badge
 
+        // find badge
         Badge b = badgeDAO.find("31A25435");
-        
+
         // output should be "Test Badge: #31A25435 (Munday, Paul J)"
-        
         System.err.println("Test Badge: " + b.toString());
-        
+
     }
 
 }
